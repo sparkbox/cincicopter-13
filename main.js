1019f4b493e2b51f085ad4222a6a55359672084a
Jeremy Mack <mack@heysparkbox.com>
2013-09-07 10:42:33 -0400

    feat: spin and land

--- main.js
+++ main.js
@@ -1,11 +1,43 @@
 var arDrone = require('ar-drone');
 var client = arDrone.createClient();
 
-client.takeoff();
+var land = function() {
+  client.after(1, function() {
+    client.land();
+  }).after(1, function() {
+    process.exit();
+  });
+};
 
-client.after(5000, function() {
-  this.clockwise(0.5);
-}).after(1000, function() {
-  this.stop();
-  this.land();
-});
+// var run = function(function)
+
+var ROTATE_SPEED = 0.8,
+SPIN_WAIT = 3000;
+
+main = function() {
+  client.takeoff();
+
+  client.after(5000, function() {
+    this.clockwise(ROTATE_SPEED);
+  }).after(SPIN_WAIT, function() {
+    this.stop();
+  }).after(1000, function() {
+    this.counterClockwise(ROTATE_SPEED);
+  }).after(SPIN_WAIT, function() {
+    this.stop();
+  }).after(50, function() {
+    this.land();
+  }).after(50, function() {
+    process.exit();
+  });
+};
+
+try {
+  main();
+} catch (e) {
+  console.log('Landing due to exception.');
+  client.land();
+  client.after(1, function() {
+    process.exit();
+  });
+}
